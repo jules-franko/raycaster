6d07f418db3aab148cb307869e4b6e108be14e48
jules-franko <julesfranko@proton.me>
2025-05-12 23:11:08 -0600

    cleaned

--- main.cc
+++ main.cc
@@ -22,8 +22,10 @@ const int screenWidth = 512;
 const int screenHeight = 512;
 
 float px, py;
-float p_angle = 0;
+float pa = 0;
 float pdx, pdy;
+float FOV = 50*PI/180;
+float num_rays = 512;
 
 int mapX=8, mapY=8, mapS=64;
 int map[] =
@@ -31,8 +33,8 @@ int map[] =
     1, 1, 1, 1, 1, 1, 1, 1,
     1, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 0, 0, 1, 1,
-    1, 0, 0, 0, 0, 0, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 1, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 1,
     1, 1, 1, 1, 1, 1, 1, 1,
@@ -49,42 +51,91 @@ void drawRays() {
     DrawLine(px, py, ray_end_x, ray_end_y, GREEN);
 }
 
-void drawRaysBad()
+// void drawRaysBad()
+// {
+//
+//     float angle_incr = FOV / num_rays;
+//     float start_angle = pa - (FOV/2);
+//
+//     float sim_angle = 0;
+//     int len;
+//     bool hit = false;
+//
+//     //Loops for the amount of rays to draw
+//     for (int j = 0; j < num_rays; j++)
+//     {
+//         sim_angle = start_angle + j * angle_incr;
+//
+//         //Draws a single line at angle
+//         for (int i = 0; i < 512; i++)
+//         {
+//             float lx = px + i * cos(sim_angle);
+//             float ly = py + i * sin(sim_angle);
+//             DrawPixel(lx, ly, GREEN);
+//             int y = int(ly/64);
+//             int x = int(lx/64);
+//
+//             if ((map[  x  ] == 1) && (map[ y*8+x ]))
+//             {
+//                 hit = true;
+//                 len = i;
+//                 break;
+//             }
+//         }
+//
+//         len *= cos(pa - sim_angle);
+//         int len2 = 512-len;
+//
+//         if (hit)
+//         {
+//             DrawLine(j, len2/2, j, len2+(len2/2), RED);
+//         }
+//
+//     }
+//
+//
+// }
+
+void drawRays3D()
 {
-    int arr[512];
+    float fov = 50*PI/180;
+    float num_rays = 512;
+    float start_angle = pa - (fov/2);
+    float angle = 0;
+    float len;
+    float render_len;
 
-    float sim_angle = p_angle - float(PI)/8;
-
-    for (int j = 0; j < 512; j+=1)
+    //Draw for entire fov
+    for (int j = 0; j < num_rays; j++)
     {
-        sim_angle += 0.001;
+        angle = start_angle + j * (fov/num_rays);
 
-        int len;
+        //Draw one line
+        for (int i =0; i < 512; i++)
+        {
+            float lx = px + i * cos(angle);
+            float ly = py + i * sin(angle);
+            DrawPixel(lx, ly, GREEN);
 
-        for (int i = 0; i < 512; i++) {
-            float lx = px + i * cos(sim_angle);
-            float ly = py + i * sin(sim_angle);
-
-            //DrawPixel(lx, ly, GREEN);
-
+            //Get the (x,y) of the newest point
             int y = int(ly/64);
             int x = int(lx/64);
 
-            if ((map[  x  ] == 1) && (map[ y*8+x ])) {
-
-                len = 512-i;
-
-
-
+            if ((map[  x  ] == 1) && (map[ y*8+x ]))
+            {
+                len = i;
+                render_len = 512 - len;
                 break;
             }
         }
 
-        DrawLine(j, len/4, j, len+(len/4), GREEN);
+        //Fix the fisheye?
+        render_len = render_len* cos(pa - angle);
+        if (render_len > 320) (render_len = 320);
 
+        //Draw the rendered line
+        DrawRectangle(j*(512/num_rays), render_len/2, (512/num_rays), render_len, RED);
     }
-
-
 }
 
 void drawMap2d() {
@@ -102,24 +153,24 @@ void drawMap2d() {
 void movePlayer() {
 
     if (IsKeyDown(KEY_W)) {
-        px += 1*cos(p_angle);
-        py += 1*sin(p_angle);
+        px += 1*cos(pa);
+        py += 1*sin(pa);
     }
     if (IsKeyDown(KEY_S)) {
-        px -= 1*cos(p_angle);
-        py -= 1*sin(p_angle);
+        px -= 1*cos(pa);
+        py -= 1*sin(pa);
     }
 
     if (IsKeyDown(KEY_D)) {
-        p_angle += 0.01;
-        if (p_angle >= 2*PI) {
-            p_angle = 0;
+        pa += 0.01;
+        if (pa >= 2*PI) {
+            pa = 0;
         }
     }
     if (IsKeyDown(KEY_A)) {
-        p_angle -= 0.01;
-        if (p_angle <= 0) {
-            p_angle = 2*PI;
+        pa -= 0.01;
+        if (pa <= 0) {
+            pa = 2*PI;
         }
     }
 }
@@ -131,8 +182,8 @@ int main(void)
 
     px = screenWidth/2;
     py = screenHeight/2;
-    pdx = cos(p_angle);
-    pdy = sin(p_angle);
+    pdx = cos(pa);
+    pdy = sin(pa);
 
     InitWindow(screenWidth, screenHeight, "Divine Intellect Raycaster");
 
@@ -145,17 +196,17 @@ int main(void)
         // Update
         // Update your variables here
         movePlayer();
-        pdx = cos(p_angle);
-        pdy = sin(p_angle);
+        pdx = cos(pa);
+        pdy = sin(pa);
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(BLACK);
-            //drawMap2d();
-            //drawPlayer();
-            drawRaysBad();
+            drawMap2d();
+            drawPlayer();
+            drawRays3D();
             //drawRays();
 
         EndDrawing();
